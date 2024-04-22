e96e117ed31bbd3e97f826642957901e53825f2e
ALK <165754555+alkoyama@users.noreply.github.com>
2024-04-22 14:01:08 +0900

    Update index_7thA.php
    symbolreplacement実装

--- index_7thA.php
+++ index_7thA.php
@@ -7,6 +7,7 @@
      <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
      <script src="https://cdn.jsdelivr.net/bxslider/4.2.12/jquery.bxslider.min.js"></script>
      <script type="text/javascript" src="./js/index_7thA.js"></script>
+         <script type="text/javascript" src="./js/symbolreplacement.js"></script>
      <title>index</title>
    </head>
    <body>
