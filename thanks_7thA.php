dc1605692ea28db330ecc7f0ebe4a9ee49143e3d
AMbidextrous-GH <165616631+AMbidextrous-GH@users.noreply.github.com>
2024-05-08 21:06:05 +0900

    thanks_7thA.phpの装飾用コード追加

--- thanks_7thA.php
+++ thanks_7thA.php
@@ -1,26 +1,29 @@
 <!DOCTYPE html>
 <html lang="ja">
 <head>
-    <meta charset="UTF-8">
-    <title>ご注文ありがとうございます</title>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css">
+  <meta charset="UTF-8">
+  <link rel="stylesheet" href="./css/reset.css">
+  <link rel="stylesheet" href="./css/thanks_7thA.css">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css">
+  <title>ご注文ありがとうございます</title>
 </head>
 <body>
-    <div class="container text-center mt-5">
-        <h1>ご注文ありがとうございました！</h1>
-        <p>またのご利用をお待ちしております。</p>
+  <div class="container text-center mt-5">
+    <div class="fade-in"><img src="./images/thanks/thanks.png" alt="ご注文感謝☆彡"></div> 
+    <div class="korokoro"><img src="./images/thanks/thanks_korokoro.gif"></div>
         
-        <?php
-        if ($_SERVER['REQUEST_METHOD'] === 'GET' && isset($_GET['oid'])) {
-            $oid = htmlspecialchars($_GET['oid']);
-            echo "<p>注文番号: <strong>$oid</strong></p>";
-        } else {
-            echo "<p>注文番号が見つかりません。</p>";
-        }
-        ?>
+    <?php
+    if ($_SERVER['REQUEST_METHOD'] === 'GET' && isset($_GET['oid'])) {
+        $oid = htmlspecialchars($_GET['oid']);
+        echo "<p>注文番号: <strong>$oid</strong></p>";
+     } else {
+        echo "<p>注文番号が見つかりません。</p>";
+     }
+    ?>
 
         <!-- ホームページに戻る -->
-        <a href="index_7thA.php" class="btn btn-primary mt-3">ストアフロントに戻る</a>
-    </div>
+    <div class="korokoro"><img src="./images/thanks/thanks_korokoro.gif"></div>
+    <a href="index_7thA.php" class="btn btn-primary ">ストアフロントに戻る</a>
+  </div>
 </body>
 </html>
