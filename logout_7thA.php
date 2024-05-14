665f7ebef345b71c67cdb0fc8aa7e03121668fdb
ALK <165754555+alkoyama@users.noreply.github.com>
2024-05-14 15:27:01 +0900

    Update logout_7thA.php

--- logout_7thA.php
+++ logout_7thA.php
@@ -49,8 +49,8 @@
      echo "ログインしてください。</p>";
      }
     ?>
-         <li><a href="./" class="header_shop"><span title="SHOP"><img src="./images/icon/index_header-shop.png" alt="shop"></span></a></li>
-         <li><a href="./" class="header_mypage"><span title="MY PAGE"><img src="./images/icon/index_header-mypage.png" alt="shop"></span></a></li>
+         <li><a href="./index_7tha.php" class="header_shop"><span title="SHOP"><img src="./images/icon/index_header-shop.png" alt="shop"></span></a></li>
+         <li><a href="./my_page_7tha.php" class="header_mypage"><span title="MY PAGE"><img src="./images/icon/index_header-mypage.png" alt="shop"></span>
          <li><a href="./" class="header_contact"><span title="CONTACT"><img src="./images/icon/index_header-contact.png" alt="shop"></span></a></li>
         </ul>
        </nav>
@@ -74,8 +74,8 @@
        <div class="footer_menu">
           <nav>
            <ul class="foot_B">
-             <li><a href="./">Shop</a></li>
-             <li><a href="./">My Page</a></li>
+             <li><a href="./index_7tha.php">Shop</a></li>
+             <li><a href="./my_page_7thA.php">My Page</a></li>
              <li><a href="./">Contact</a></li>
            </ul>
           </nav>
