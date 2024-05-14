622a31a1a0ed2f5f76864168fb8d7b935df99dff
ALK <165754555+alkoyama@users.noreply.github.com>
2024-05-14 14:55:57 +0900

    Revert "Update login_7thA.php"

--- signup_7thA.php
+++ signup_7thA.php
@@ -1,47 +1,11 @@
 <!DOCTYPE html>
- <html lang="ja">
-   <head>
-     <meta charset="UTF-8">
-     <link rel="stylesheet" href="./css/index_7thA.css">
-     <link rel="stylesheet" href="./css/style.css">
-     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.css">
-     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.9.0/slick-theme.css">
-     <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
-     <script src="https://use.fontawesome.com/926fe18a63.js"></script>
-     <title>index</title>
-   </head>
-   <body>
-    <header class="header" id="header">
-      <div class="header_container">
-       <div class="header_container_small">
-        <a class="head_A">
-         <img src="./images/icon/index_header-ball.gif" alt="ゆらゆら">
-        </a>
-        <button type="button" class="head_C">
-         <span class="fa fa-bars" title="MEMU"></span><span class="sr-only">MEMU</span>
-        </button>
-       </div>
-       <nav class="head_B">
-        <ul>
-                 <?php
-             session_start();
-             if (isset($_SESSION["loggedIn"]) && $_SESSION["loggedIn"] === true) {
-              echo "<p>ID: " . $_SESSION["CID"] . "</p>";
-    
-    } else {
-     // User is not logged in, redirect to login page or display a login prompt
-     echo "ログインしてください。</p>";
-     }
-    ?>
-         <li><a href="./index_7tha.php" class="header_shop"><span title="SHOP"><img src="./images/icon/index_header-shop.png" alt="shop"></span></a></li>
-         <li><a href="./my_page_7tha.php" class="header_mypage"><span title="MY PAGE"><img src="./images/icon/index_header-mypage.png" alt="shop"></span></a></li>
-         <li><a href="./" class="header_contact"><span title="CONTACT"><img src="./images/icon/index_header-contact.png" alt="shop"></span></a></li>
-        </ul>
-       </nav>
-      </div>
-    </header>
-       
-
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Create New Account</title>
+</head>
+<body>
     <h1>Create New Account</h1>
     <form action="" method="post"> <label for="name">名前:</label>
         <input type="text" name="name" id="name" required><br>
@@ -120,24 +84,5 @@
               }
         }
     ?>
-
-       
-    <footer class="footer" id="footer">
-       <div class="footer_menu">
-          <nav>
-           <ul class="foot_B">
-             <li><a href="./">Shop</a></li>
-             <li><a href="./">My Page</a></li>
-             <li><a href="./">Contact</a></li>
-           </ul>
-          </nav>
-         <div class="foot_C"><p class="copyright">&copy; JS7th_teamA 2024</p></div>
-       </div>
-       <div class="foot_A"><img src="./images/icon/index_footer-ball.gif" alt="ころころ"></div>
-    </footer>
-  
-     <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
-     <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js"></script>
-     <script type="text/javascript" src="./js/index_7thA.js"></script>
-   </body>
-  </html>
+</body>
+</html>
